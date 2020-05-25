a5f6d9180f50b017015115d2472cbe518bb10818
nodm <nodm.pe@gmail.com>
2020-05-25 10:24:06 +0300

    NGCC: - delete template files

--- projects/ng-config-cat/src/public-api.ts
+++ projects/ng-config-cat/src/public-api.ts
@@ -2,6 +2,3 @@
  * Public API Surface of ng-config-cat
  */
 
-export * from './lib/ng-config-cat.service';
-export * from './lib/ng-config-cat.component';
-export * from './lib/ng-config-cat.module';
